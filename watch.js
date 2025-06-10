f9310a9e93111249a4b96fd6a8b1faa6a853666b
ahmed <ashams3719262@gmail.com>
2025-06-10 20:06:34 +0300

    adding vercel requirements

--- watch.js
+++ watch.js
@@ -2,8 +2,5 @@ const { exec } = require('child_process');
 const nodemon = require('nodemon');
 
 nodemon({
-  watch: ['**/*'],
-  ignore: ['node_modules', 'public/img'],
-  ext: 'js,html,css,json',
-  exec: '.\\auto-push.bat' // Use 'exec' to run the batch file
+
 });
